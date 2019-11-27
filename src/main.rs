f9c3f7fb3554811dbead77e793652339a2bb9c24
Max Dobeck <msdobeck@gmail.com>
2019-11-26 18:10:10 -0800

    Added basic clap setup in main.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate clap;
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let user_commands = App::new("MyApp")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author("Max Dobeck. <max.dobeck@protonmail.com>")
+        .about("Pull useful data out of some source.")
+        .arg(
+            Arg::with_name("version")
+                .long("version")
+                .help("Print the current version of Gimme")
+                .takes_value(false),
+        )
+        .get_matches();
+
+        if user_commands.is_present("version") {
+            println!("gimme version {}", env!("CARGO_PKG_VERSION"))
+        }
 }

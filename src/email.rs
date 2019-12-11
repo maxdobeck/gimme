3815b08242689fdffae7fd55b44f8ddbb5d07f80
Max Dobeck <msdobeck@gmail.com>
2019-12-10 20:54:57 -0800

    Extended String and &str to use the is_email() function.

--- src/email.rs
+++ src/email.rs
@@ -1,2 +1,27 @@
-use super::sources;
 
+pub trait StringExt {
+    fn is_email(&self) -> bool;
+}
+
+impl StringExt for String {
+    fn is_email(&self) -> bool {
+        return false
+    }
+}
+
+impl StringExt for &str {
+    fn is_email(&self) -> bool {
+        return false
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::StringExt;
+
+    #[test]
+    fn should_not_be_email() {
+        assert_eq!("hello".to_string().is_email(), false);
+        assert_eq!("hello again".is_email(), false)
+    }
+}

d4728097f65d0cb0854a07ef209e67197a9c9f98
Max Dobeck <msdobeck@gmail.com>
2019-12-28 00:00:25 -0800

    Removed code that was added prematurely.

--- src/main.rs
+++ src/main.rs
@@ -22,26 +22,6 @@ fn main() {
                 .takes_value(false)
                 .help("Find all emails or email approximates."),
         )
-        // .arg(
-        //     Arg::with_name("phone")
-        //         .long("phone")
-        //         .multiple(false)
-        //         .takes_value(false)
-        //         .help("Find all phone numbers."),
-        // )
-        // .arg(
-        //     Arg::with_name("error")
-        //         .long("error")
-        //         .takes_value(false)
-        //         .multiple(false)
-        //         .help("Find any common errors."),
-        // )
-        .arg(
-            Arg::with_name("source")
-                .long("source")
-                .short("s")
-                .help("The source you want to search in.  Defaults to your clipboard."),
-        )
         .get_matches();
 
     if cmds.is_present("version") {
@@ -51,14 +31,12 @@ fn main() {
     let cb = sources::get_clipboard();
 
     if cmds.is_present("email") {
-        if !cmds.is_present("source") {
-            match cb.len() {
-                0 => println!("Clipboard is empty!"),
-                _ => cb
-                    .split_whitespace()
-                    .enumerate()
-                    .for_each(|(_, word)| println!("{}", word)),
-            }
-        };
-    }
+        match cb.len() {
+            0 => println!("Clipboard is empty!"),
+            _ => cb
+                .split_whitespace()
+                .enumerate()
+                .for_each(|(_, word)| println!("{}", word)),
+        }
+    };
 }

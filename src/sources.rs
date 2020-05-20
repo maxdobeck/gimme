078a1c3960e9650d9e2a1dcc53e76c7c8ed75501
Max Dobeck <msdobeck@gmail.com>
2020-05-19 18:47:21 -0700

    Updated documentation for sources

--- src/sources.rs
+++ src/sources.rs
@@ -2,6 +2,8 @@ extern crate clipboard;
 use clipboard::ClipboardContext;
 use clipboard::ClipboardProvider;
 
+/// Get the contents of the clipboard.  Works on all systems but may require 
+/// dependencies on x11 in linux environments.
 pub fn get_clipboard() -> String {
     let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
     match ctx.get_contents() {
